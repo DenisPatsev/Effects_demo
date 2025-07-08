dd9e8e51bd9c2dc47caba709fe289ca1c26224f5
Den Patsev <patsev.den@mail.ru>
2025-07-08 22:38:06 +0300

    microupd

--- Assets/SteelBallEffect.cs
+++ Assets/SteelBallEffect.cs
@@ -5,6 +5,7 @@ public class SteelBallEffect : MonoBehaviour
 {
     [SerializeField] private Renderer _renderer;
     [SerializeField] private ParticleSystem _groundDamage;
+    [SerializeField] private ParticleSystem _smoke;
 
     [ColorUsage(true, true)] [SerializeField]
     private Color _startColor;
@@ -15,15 +16,24 @@ public class SteelBallEffect : MonoBehaviour
     [SerializeField] private float _colorChangeSpeed;
 
     private MaterialPropertyBlock _materialPropertyBlock;
+    private bool _isCollided;
 
     private void Start()
     {
         _materialPropertyBlock = new MaterialPropertyBlock();
+        _isCollided = false;
     }
 
     private void OnCollisionEnter(Collision other)
     {
         _groundDamage.Play();
+        _smoke.Play();
+
+        if (!_isCollided)
+        {
+            transform.position += new Vector3(0, 0.25f, 0);
+            _isCollided = true;
+        }
     }
 
     private void FixedUpdate()
